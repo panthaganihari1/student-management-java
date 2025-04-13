2766c1a4e30ac70af3e3dfcf918165fc2837ac41
Gani Hari <panthaganihari1@gmail.com>
2025-04-13 23:06:31 +0530

    "Removed the Un necassary comments"

--- src/main/java/com/hari/studentmanagement/controller/StudentController.java
+++ src/main/java/com/hari/studentmanagement/controller/StudentController.java
@@ -3,12 +3,9 @@ package com.hari.studentmanagement.controller;
 import com.hari.studentmanagement.entity.Student;
 import com.hari.studentmanagement.service.StudentService;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
